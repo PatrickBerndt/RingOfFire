ee90ab7c9617a5cb4afca12cc89cc26911adeaf3
Patrick Berndt <122051407+PatrickBerndt@users.noreply.github.com>
2023-06-10 12:08:52 +0200

    added active player highlight

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -8,6 +8,6 @@ import { Component, Input } from '@angular/core';
 export class PlayerComponent {
 
   @Input()  name: string ;
-  @Input()  activePlayer: boolean = false;
+  @Input()  playerActive: boolean = false;
 
 }
